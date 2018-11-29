90c3f7064d996012c74ee92d3be89955ba931a48
youngju <youngjucha0316@gmail.com>
2018-11-29 17:57:55 +0900

    동우등록

--- app/src/main/java/com/devstories/anipointcompany/android/activities/SettingFragment.kt
+++ app/src/main/java/com/devstories/anipointcompany/android/activities/SettingFragment.kt
@@ -1,20 +1,20 @@
 package com.devstories.anipointcompany.android.activities
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.app.Fragment
+import android.app.ProgressDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
 import com.devstories.anipointcompany.android.R
 import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : android.support.v4.app.Fragment() {
 
     lateinit var myContext: Context
-
+    private var progressDialog: ProgressDialog? = null
+    lateinit var myInfoTV: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -26,11 +26,27 @@ class SettingFragment : android.support.v4.app.Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        myInfoTV = view.findViewById(R.id.myInfoTV)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val SettingMyInfoFragment : SettingMyInfoFragment = SettingMyInfoFragment()
+
+        myInfoTV.setOnClickListener {
+            //프레그먼트에서 프래그먼트선언하기
+            childFragmentManager.beginTransaction().replace(R.id.settingFL, SettingMyInfoFragment).commit()
+        }
+
+
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (progressDialog != null) {
+            progressDialog!!.dismiss()
+        }
+    }
 
 }
--- app/src/main/java/com/devstories/anipointcompany/android/activities/SettingMyInfoFragment.kt
+++ app/src/main/java/com/devstories/anipointcompany/android/activities/SettingMyInfoFragment.kt
@@ -1,12 +1,12 @@
 package com.devstories.anipointcompany.android.activities
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
 import com.devstories.anipointcompany.android.R
 
@@ -14,11 +14,52 @@ class SettingMyInfoFragment : Fragment() {
 
     lateinit var myContext: Context
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    lateinit var compNameET: EditText
+    lateinit var phoneNum1ET: EditText
+    lateinit var termET: EditText
+    lateinit var compIdET: EditText
+    lateinit var addImage1RL: EditText
+    lateinit var addImage2RL: EditText
+    lateinit var addImage3RL: EditText
+    lateinit var tempPasswordET: EditText
+    lateinit var newPasswordET: EditText
+    lateinit var newPassCheckET: EditText
+    lateinit var checkTV: EditText
+ 
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         this.myContext = container!!.context
 
         return inflater.inflate(R.layout.fragment_setting_my_info, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        compNameET = view.findViewById(R.id.compNameET)
+        phoneNum1ET = view.findViewById(R.id.phoneNum1ET)
+        compIdET = view.findViewById(R.id.compIdET)
+        addImage1RL = view.findViewById(R.id.addImage1RL)
+        addImage2RL = view.findViewById(R.id.addImage2RL)
+        termET = view.findViewById(R.id.termET)
+        addImage1RL = view.findViewById(R.id.addImage1RL)
+        addImage2RL = view.findViewById(R.id.addImage2RL)
+        addImage3RL = view.findViewById(R.id.addImage3RL)
+        tempPasswordET = view.findViewById(R.id.tempPasswordET)
+        newPasswordET = view.findViewById(R.id.newPasswordET)
+        newPassCheckET = view.findViewById(R.id.newPassCheckET)
+        checkTV = view.findViewById(R.id.checkTV)
+
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+
+
+
 }
