118efe8ac170820de978800fe1727863637464f1
admin <chashin22>
2018-11-30 18:20:35 +0900

    신행 올림

---
+++ app/src/main/java/com/devstories/anipointcompany/android/activities/MssgAnalysisFragment.kt
@@ -0,0 +1,21 @@
+package com.devstories.anipointcompany.android.activities
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.devstories.anipointcompany.android.R
+
+class MssgAnalysisFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fra_mssg_analysis, container, false)
+    }
+}
