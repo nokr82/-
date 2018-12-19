5d84e32bd81ecc90c15fd3cf319f4d6d176fd91f
youngju <youngjucha0316@gmail.com>
2018-12-20 00:06:08 +0900

    동우등록

--- app/src/main/java/com/devstories/anipointcompany/android/activities/SettingMyInfoFragment.kt
+++ app/src/main/java/com/devstories/anipointcompany/android/activities/SettingMyInfoFragment.kt
@@ -46,6 +46,7 @@ class SettingMyInfoFragment : Fragment() {
     lateinit var checkTV: TextView
     lateinit var infocheckTV: TextView
     lateinit var imgcheckTV: TextView
+    lateinit var termTV: TextView
     lateinit var userLL: LinearLayout
 
 
@@ -75,7 +76,7 @@ class SettingMyInfoFragment : Fragment() {
         phoneNum3ET = view.findViewById(R.id.phoneNum3ET)
         compIdET = view.findViewById(R.id.compIdET)
         addImage1RL = view.findViewById(R.id.addImage1RL)
-        termET = view.findViewById(R.id.termET)
+        termTV = view.findViewById(R.id.termTV)
         tempPasswordET = view.findViewById(R.id.tempPasswordET)
         newPasswordET = view.findViewById(R.id.newPasswordET)
         newPassCheckET = view.findViewById(R.id.newPassCheckET)
@@ -83,6 +84,7 @@ class SettingMyInfoFragment : Fragment() {
         infocheckTV= view.findViewById(R.id.infocheckTV)
         imgcheckTV= view.findViewById(R.id.imgcheckTV)
         userLL =  view.findViewById(R.id.userLL)
+        termTV=  view.findViewById(R.id.termTV)
 
 
 
@@ -227,13 +229,17 @@ class SettingMyInfoFragment : Fragment() {
                         val phone2 = Utils.getString(company,"phone2")
                         val phone3 = Utils.getString(company,"phone3")
                         val login_id = Utils.getString(company,"login_id")
-                         passwd=Utils.getString(company,"passwd")
+                        var s_contract_term = Utils.getString(company,"s_contract_term")
+                        var e_contract_term = Utils.getString(company,"e_contract_term")
+
+                        passwd=Utils.getString(company,"passwd")
 
                         compNameET.setText(company_name)
                         phoneNum1ET.setText(phone1)
                         phoneNum2ET.setText(phone2)
                         phoneNum3ET.setText(phone3)
                         compIdET.setText(login_id)
+                        termTV.text = s_contract_term+"~"+e_contract_term
 
                         val images = response.getJSONArray("images")
                         Log.d("이미지",images.toString())
