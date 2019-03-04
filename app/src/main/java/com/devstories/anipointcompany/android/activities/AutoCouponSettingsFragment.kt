f61b91e93e7e0b3b975106ecf77242d03e35793c
youngju <youngjucha0316@gmail.com>
2019-03-04 17:00:42 +0900

    동우등록

--- app/src/main/java/com/devstories/anipointcompany/android/activities/AutoCouponSettingsFragment.kt
+++ app/src/main/java/com/devstories/anipointcompany/android/activities/AutoCouponSettingsFragment.kt
@@ -1,6 +1,5 @@
 package com.devstories.anipointcompany.android.activities
 
-import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
